ea79414628fdc9093ccaf0e58b8716de2da47835
Andreas Gunnarsson <tailsfang@gmail.com>
2021-11-08 10:42:16 +0100

    La till en testklass/model

---
+++ test1/Models/AndreasClass8okt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test1.Models
+{
+    public class AndreasClass8okt
+    {
+        //hej å hå
+    }
+}
